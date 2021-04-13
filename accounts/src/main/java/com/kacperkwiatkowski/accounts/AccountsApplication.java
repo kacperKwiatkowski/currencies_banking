d1faf0d3dfa49b101d0251da18043f60ff38e269
Kacper Kwiatkowski <kacper18k@gmail.com>
2021-04-13 18:53:37 +0200

    Eureka set-up completed.

---
+++ accounts/src/main/java/com/kacperkwiatkowski/accounts/AccountsApplication.java
@@ -0,0 +1,15 @@
+package com.kacperkwiatkowski.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
+
+}
