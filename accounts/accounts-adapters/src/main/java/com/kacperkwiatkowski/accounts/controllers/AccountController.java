406212caa2f2572453e8ad4cfe8fc6b11efbb584
Kacper Kwiatkowski <kacper18k@gmail.com>
2021-04-20 11:20:20 +0200

    Accounts architecture set.

---
+++ accounts/accounts-adapters/src/main/java/com/kacperkwiatkowski/accounts/controllers/AccountController.java
@@ -0,0 +1,17 @@
+package com.kacperkwiatkowski.accounts.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/getAll")
+    ResponseEntity<String> getAllUsers(){
+        return ResponseEntity.ok("asd");
+    }
+}
