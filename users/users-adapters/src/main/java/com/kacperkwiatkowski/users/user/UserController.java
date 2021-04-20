7e3bbea9a2fd45684b6da37b6e5c9e20d048ee29
Kacper Kwiatkowski <kacper18k@gmail.com>
2021-04-20 12:16:34 +0200

    Minor improvements.

--- users/users-adapters/src/main/java/com/kacperkwiatkowski/users/user/UserController.java
+++ users/users-adapters/src/main/java/com/kacperkwiatkowski/users/user/UserController.java
@@ -1,6 +1,5 @@
 package com.kacperkwiatkowski.users.user;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
-@Slf4j
 public class UserController {
 
     @GetMapping("/getAll")
-    ResponseEntity<UserDto> getAllUsers(){
-        log.debug("TEST");
-        return ResponseEntity.ok(UserDto.builder().email("test@gmail.com").build());
+    ResponseEntity<String> getAllUsers(){
+        return ResponseEntity.ok("USERS");
     }
 }
