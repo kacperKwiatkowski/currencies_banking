0009331e1ff82f516bb2b70af9c6299e3d1acd7a
Kacper Kwiatkowski <kacper18k@gmail.com>
2021-04-21 09:56:30 +0200

    Accounts API routing fixed.

--- accounts/accounts-adapters/src/main/java/com/kacperkwiatkowski/accounts/controllers/Controller.java
+++ accounts/accounts-adapters/src/main/java/com/kacperkwiatkowski/accounts/controllers/Controller.java
@@ -6,11 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/account")
+@RequestMapping("/accounts")
 class Controller {
 
     @GetMapping("/getAll")
     ResponseEntity<String>getAll(){
         return ResponseEntity.ok("ACCOUNT");
+    }
+
+
+    @GetMapping("/test")
+    ResponseEntity<String>test(){
+        return ResponseEntity.ok("ACCOUNT-TEST");
     }
 }
